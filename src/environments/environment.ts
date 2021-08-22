f371df53a7ac8ca5c9f8a7625202f23b40bc1de8
Hemang Murugan <52290764+iamtheretronerd@users.noreply.github.com>
2021-08-22 19:08:24 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,15 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAI9sWxdYU3H6l9BNDGZFyO_Vwca9jD8Z0",
-    authDomain: "beta-b6701.firebaseapp.com",
-    projectId: "beta-b6701",
-    storageBucket: "beta-b6701.appspot.com",
-    messagingSenderId: "827500141650",
-    appId: "1:827500141650:web:d3a230da56a2841315d38b",
-    measurementId: "G-7B93J0EFEH"
+    apiKey: "your api key",
+    authDomain: "auth domain",
+    projectId: "project id",
+    storageBucket: " storage bucket",
+    messagingSenderId: " message sender id ",
+    appId: "app id",
+    measurementId: "mes id"
   },
-  agoraAppId: '1075a42f93ee41869b49b758f782265a'
+  agoraAppId: 'agora app id'
 };
 
 /*
